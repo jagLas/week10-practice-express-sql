c5d6fe9a0b823b5c78c0c051402ab1dc4099a982
jagLas <jglasher@gmail.com>
2023-03-20 14:14:49 -0400

    more practice

--- installing-express-practice/app.js
+++ installing-express-practice/app.js
@@ -3,11 +3,38 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.end('hi')
+    res.send('hi')
 })
 
 app.get('/status', (req, res) => {
-    res.end('The server is alive');
+    res.send('The server is alive');
+})
+
+app.get('/json', (req, res) => {
+    const resp = {
+        property1: "value1",
+        property2: "value2"
+    };
+    res.json(resp);
+})
+
+const data = {
+    users: []
+}
+
+// Use POST to add a user
+app.post('/users', (req, res) => {
+    let newUser = {
+        name: 'Phyllis',
+        age: 68
+    }
+    data.users.push(newUser)
+    res.send(newUser);
+});
+
+// Use GET to retrieve a list of all users
+app.get('/users', (req, res) => {
+    res.send(data.users);
 })
 
 const port = 5000;
