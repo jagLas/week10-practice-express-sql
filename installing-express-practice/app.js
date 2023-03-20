d101a396a93008d0a3a4648fb57b930eee57abca
jagLas <jglasher@gmail.com>
2023-03-20 13:27:26 -0400

    continue initial set up

--- installing-express-practice/app.js
+++ installing-express-practice/app.js
@@ -1,3 +1,10 @@
 const express = require('express');
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.end('Hello there!')
+})
+
+const port = 5000;
+app.listen(port, () => console.log(`Server is listening on port ${port}.`));
