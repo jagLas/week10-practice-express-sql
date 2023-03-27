dd57f9a2db866f4eafe8d8df9e3925c2ae838af5
jagLas <jglasher@gmail.com>
2023-03-27 15:04:28 -0400

    finished phase 2 and 3

--- practice-for-week-10-express-static-files/server/app.js
+++ practice-for-week-10-express-static-files/server/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 // Your code here
-app.use(express.static('assets'));
+// app.use(express.static('assets'));
+app.use(express.static('assets/scripts'))
+app.use('/stylesheets', express.static('assets/css'))
 
 
 const port = 5000;
