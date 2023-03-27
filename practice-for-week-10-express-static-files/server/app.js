e8aba5a668b0cd2537103823b8cdab44f9533d7f
jagLas <jglasher@gmail.com>
2023-03-27 15:05:52 -0400

    finished bonus

--- practice-for-week-10-express-static-files/server/app.js
+++ practice-for-week-10-express-static-files/server/app.js
@@ -4,7 +4,7 @@ const app = express();
 // app.use(express.static('assets'));
 app.use(express.static('assets/scripts'))
 app.use('/stylesheets', express.static('assets/css'))
-
+app.use('/stickers', express.static('assets/images'))
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
