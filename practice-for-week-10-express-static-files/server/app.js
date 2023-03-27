09f2703adeb89b0aab21e090c5f551aecf342640
jagLas <jglasher@gmail.com>
2023-03-27 15:01:31 -0400

    phase 1 done

--- practice-for-week-10-express-static-files/server/app.js
+++ practice-for-week-10-express-static-files/server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 // Your code here
+app.use(express.static('assets'));
+
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
