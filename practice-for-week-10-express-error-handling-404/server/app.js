356f63b80c0a71538423156f42808f102a4a5996
jagLas <jglasher@gmail.com>
2023-03-28 10:02:49 -0400

    practice complete

--- practice-for-week-10-express-error-handling-404/server/app.js
+++ practice-for-week-10-express-error-handling-404/server/app.js
@@ -5,5 +5,20 @@ app.get('/', (req, res) => {
   res.send('GET / This is the root URL');
 });
 
+app.use((req, res, next) => {
+  const err = new Error(`Sorry, the requested resource couldn't be found`);
+  err.status = 404;
+  next(err);
+})
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.body = {
+    message: err.message,
+    statusCode: err.status
+  };
+  res.status(err.status || 500).json(res.body)
+})
+
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
