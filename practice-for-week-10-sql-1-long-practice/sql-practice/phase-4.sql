9dd15408c5b089002edabfd730d9b6c8558f3171
jagLas <jglasher@gmail.com>
2023-03-30 15:13:24 -0400

    started step 3

--- practice-for-week-10-sql-1-long-practice/sql-practice/phase-4.sql
+++ practice-for-week-10-sql-1-long-practice/sql-practice/phase-4.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS perf_reviews;
 DROP TABLE IF EXISTS parties;
 
 CREATE TABLE `employees` (
-  `id` INTEGER PRIMARY KEY,
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `first_name` VARCHAR(50) NOT NULL,
   `last_name` VARCHAR(50) NOT NULL,
   `department` VARCHAR(50),
@@ -14,7 +14,7 @@ CREATE TABLE `employees` (
 );
 
 CREATE TABLE `relationships` (
-  `id` INTEGER PRIMARY KEY,
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `employee1` INTEGER,
   `employee2` INTEGER,
   FOREIGN KEY (employee1) REFERENCES employees(id),
@@ -22,15 +22,26 @@ CREATE TABLE `relationships` (
 );
 
 CREATE TABLE `perf_reviews` (
-  `id` INTEGER PRIMARY KEY,
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `employee_id` INTEGER,
   `score` NUMERIC(3, 1) CHECK(score <= 10 AND score >= 0),
   FOREIGN KEY (employee_id) REFERENCES employees(id)
 );
 
 CREATE TABLE `parties` (
-  `id` INTEGER PRIMARY KEY,
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `budget` NUMERIC(9, 2),
   `onsite` BOOLEAN
 );
 
+-- Events 1-7
+INSERT INTO employees (first_name, last_name, department, role) VALUES
+    ('Michael', 'Scott', 'Management', 'Regional Manager'),
+    ('Dwight', 'Schrute', 'Sales', 'Assitant Regional Manager'),
+    ('Jim', 'Halpert', 'Sales', 'Sales Representative'),
+    ('Pam', 'Beesly', 'Reception', 'Receptionist'),
+    ('Kelly', 'Kapoor', 'Product Oversight', 'Customer Service Representative'),
+    ('Angela', 'Martin', 'Accounting', 'Head of Accounting'),
+    ('Roy', 'Anderson', 'Warehouse', 'Warehouse Staff');
+
+-- Event 8
