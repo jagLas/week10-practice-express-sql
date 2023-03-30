7e03a18ba5e1305d6c66fda14f44c08a74ed5a29
jagLas <jglasher@gmail.com>
2023-03-30 13:40:12 -0400

    phase 2 complete

--- practice-for-week-10-sql-1-long-practice/sql-practice/phase-2.sql
+++ practice-for-week-10-sql-1-long-practice/sql-practice/phase-2.sql
@@ -1,1 +1,18 @@
 -- Your code here
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS coffee_orders;
+
+CREATE TABLE customers (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(40),
+    phone NUMERIC(10, 0),
+    email VARCHAR(255),
+    points INTEGER DEFAULT 5,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE coffee_orders (
+    id INT PRIMARY KEY,
+    is_redeemed BOOLEAN DEFAULT 0,
+    ordered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
