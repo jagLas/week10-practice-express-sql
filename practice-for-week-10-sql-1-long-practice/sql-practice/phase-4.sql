2af597fce769664aacfba18689e2df781f6c0222
jagLas <jglasher@gmail.com>
2023-03-30 15:07:26 -0400

    phase 3 done of practice

--- practice-for-week-10-sql-1-long-practice/sql-practice/phase-4.sql
+++ practice-for-week-10-sql-1-long-practice/sql-practice/phase-4.sql
@@ -1,1 +1,36 @@
 -- Your code here
+
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS relationships;
+DROP TABLE IF EXISTS perf_reviews;
+DROP TABLE IF EXISTS parties;
+
+CREATE TABLE `employees` (
+  `id` INTEGER PRIMARY KEY,
+  `first_name` VARCHAR(50) NOT NULL,
+  `last_name` VARCHAR(50) NOT NULL,
+  `department` VARCHAR(50),
+  `role` VARCHAR(50)
+);
+
+CREATE TABLE `relationships` (
+  `id` INTEGER PRIMARY KEY,
+  `employee1` INTEGER,
+  `employee2` INTEGER,
+  FOREIGN KEY (employee1) REFERENCES employees(id),
+  FOREIGN KEY (employee2) REFERENCES employees(id)
+);
+
+CREATE TABLE `perf_reviews` (
+  `id` INTEGER PRIMARY KEY,
+  `employee_id` INTEGER,
+  `score` NUMERIC(3, 1) CHECK(score <= 10 AND score >= 0),
+  FOREIGN KEY (employee_id) REFERENCES employees(id)
+);
+
+CREATE TABLE `parties` (
+  `id` INTEGER PRIMARY KEY,
+  `budget` NUMERIC(9, 2),
+  `onsite` BOOLEAN
+);
+
