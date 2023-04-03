1da083df0e0e45f579f9b612269337f08a4b784c
jagLas <jglasher@gmail.com>
2023-04-03 13:10:16 -0400

    phase 3 passes spec

--- practice-for-week-10-sql-2-long-practice/sql-practice/phase-3/query-1.sql
+++ practice-for-week-10-sql-2-long-practice/sql-practice/phase-3/query-1.sql
@@ -1,2 +1,7 @@
 -- Find Hermione's cats
 -- Your code here
+
+SELECT name FROM owners
+    JOIN cat_owners ON owner_id = owners.id
+    JOIN cats ON cats.id = cat_id
+    WHERE first_name = 'Hermione';
--- practice-for-week-10-sql-2-long-practice/sql-practice/phase-3/query-2.sql
+++ practice-for-week-10-sql-2-long-practice/sql-practice/phase-3/query-2.sql
@@ -1,2 +1,8 @@
 -- Find All the Toys for Hermione's cats
 -- Your code here
+
+SELECT toys.name FROM owners
+    JOIN cat_owners ON owner_id = owners.id
+    JOIN cats ON cats.id = cat_owners.cat_id
+    JOIN toys ON toys.cat_id = cats.id
+    WHERE first_name = 'Hermione';
