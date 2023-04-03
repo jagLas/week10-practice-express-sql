2ce04d4890cefcbafcbadebd5c9ab5a7f025a2c0
jagLas <jglasher@gmail.com>
2023-04-03 13:14:48 -0400

    phase 3 with 2 JOINS

--- practice-for-week-10-sql-2-long-practice/sql-practice/phase-3/query-2.sql
+++ practice-for-week-10-sql-2-long-practice/sql-practice/phase-3/query-2.sql
@@ -1,8 +1,15 @@
 -- Find All the Toys for Hermione's cats
 -- Your code here
 
-SELECT toys.name FROM owners
-    JOIN cat_owners ON owner_id = owners.id
-    JOIN cats ON cats.id = cat_owners.cat_id
-    JOIN toys ON toys.cat_id = cats.id
-    WHERE first_name = 'Hermione';
+-- 3 JOINS
+-- SELECT toys.name FROM owners
+--     JOIN cat_owners ON owner_id = owners.id
+--     JOIN cats ON cats.id = cat_owners.cat_id
+--     JOIN toys ON toys.cat_id = cats.id
+--     WHERE first_name = 'Hermione';
+
+-- 2 JOINS
+SELECT toys.name FROM toys
+    JOIN cat_owners ON toys.cat_id = cat_owners.cat_id
+    JOIN owners ON owner_id = owners.id
+    WHERE owners.first_name = 'Hermione';
