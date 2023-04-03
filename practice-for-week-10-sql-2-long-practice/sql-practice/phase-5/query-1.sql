6f7c2a2c1e028032f7a90117efe9f200fa69460a
jagLas <jglasher@gmail.com>
2023-04-03 13:37:23 -0400

    phase 5 spec passed

--- practice-for-week-10-sql-2-long-practice/sql-practice/phase-5/query-1.sql
+++ practice-for-week-10-sql-2-long-practice/sql-practice/phase-5/query-1.sql
@@ -1,2 +1,5 @@
 -- Find the first owner that has a last name containing the lowercase letter "r"
 -- Your code here
+SELECT * FROM owners WHERE last_name 
+    LIKE '%r%' ORDER BY id
+    LIMIT 1;
--- practice-for-week-10-sql-2-long-practice/sql-practice/phase-5/query-5.sql
+++ practice-for-week-10-sql-2-long-practice/sql-practice/phase-5/query-5.sql
@@ -1,2 +1,7 @@
 -- Find names of the cats whose owners are both George Beatty and Melynda Abshire, or just George Beatty, or just Melynda Abshire
 -- Your code here
+SELECT DISTINCT name FROM owners
+    JOIN cat_owners ON owner_id = owners.id
+    JOIN cats ON cat_id = cats.id
+    WHERE first_name IN ('George', 'Melynda')
+    AND last_name IN ('Beatty', 'Abshire');
