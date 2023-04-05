1176e7f01e277252ef6aa071cc164ef6fe0ef99d
jagLas <jglasher@gmail.com>
2023-04-05 15:03:28 -0400

    fixed solution to phase 3

--- practice-for-week-10-sql-3-long-practice/sql-practice/phase-3.sql
+++ practice-for-week-10-sql-3-long-practice/sql-practice/phase-3.sql
@@ -1,2 +1,14 @@
 -- Find the name of the cats co-owned by both George Beatty and Melynda Abshire
 -- Your code here
+SELECT cats.name
+    FROM cats WHERE id IN (
+        SELECT cat_id FROM cat_owners WHERE owner_id = (
+            SELECT id FROM owners WHERE first_name = 'George' AND last_name = 'Beatty'
+        )
+
+        INTERSECT
+        
+        SELECT cat_id FROM cat_owners WHERE owner_id = (
+            SELECT id FROM owners WHERE first_name = 'Melynda' AND last_name = 'Abshire'
+        )
+    );
