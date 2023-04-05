f282a350dfb649dd6c33c3899b56bf2c3005678d
jagLas <jglasher@gmail.com>
2023-04-05 15:07:38 -0400

    added alternate solution

--- practice-for-week-10-sql-3-long-practice/sql-practice/phase-3.sql
+++ practice-for-week-10-sql-3-long-practice/sql-practice/phase-3.sql
@@ -12,3 +12,16 @@ SELECT cats.name
             SELECT id FROM owners WHERE first_name = 'Melynda' AND last_name = 'Abshire'
         )
     );
+
+-- Alternate solution
+SELECT cats.name
+    FROM cats WHERE id IN (
+        SELECT cat_id FROM cat_owners WHERE owner_id = (
+            SELECT id FROM owners WHERE first_name = 'George' AND last_name = 'Beatty'
+        )
+    )
+    AND id in (
+        SELECT cat_id FROM cat_owners WHERE owner_id = (
+            SELECT id FROM owners WHERE first_name = 'Melynda' AND last_name = 'Abshire'
+        )
+    );
