3b35944af9a8d47711dff66456445b0ab6a54333
jagLas <jglasher@gmail.com>
2023-04-05 13:53:51 -0400

    router for phase 2

--- practice-for-week-10-combining-express-sql-long-practice/server/routes/trees.js
+++ practice-for-week-10-combining-express-sql-long-practice/server/routes/trees.js
@@ -8,6 +8,8 @@ const router = express.Router();
  *   - Database permissions: read/write records in tables
  */
 // Your code here
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.DATA_SOURCE, sqlite3.OPEN_READWRITE);
 
 /**
  * BASIC PHASE 2, Step B - List of all trees in the database
@@ -20,6 +22,17 @@ const router = express.Router();
  *   - Ordered by the height_ft from tallest to shortest
  */
 // Your code here
+router.get('/', (req, res, next) => {
+    const sql = 'SELECT id, tree FROM trees ORDER BY height_ft DESC;';
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            next(err);
+        } else {
+            res.json(rows);
+        }
+    })
+})
 
 /**
  * BASIC PHASE 3 - Retrieve one tree with the matching id
