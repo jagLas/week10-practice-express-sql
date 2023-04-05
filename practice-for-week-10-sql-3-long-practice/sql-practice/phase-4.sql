10406be4a3a7e8a1cb1d09db784443ad856ac302
jagLas <jglasher@gmail.com>
2023-04-05 15:18:34 -0400

    phase 4 done

--- practice-for-week-10-sql-3-long-practice/sql-practice/phase-4.sql
+++ practice-for-week-10-sql-3-long-practice/sql-practice/phase-4.sql
@@ -1,5 +1,10 @@
 -- Give "Red" the cat one of every toy the other cats have
 -- Your code here
+INSERT INTO toys (name, cat_id)
+SELECT name, (SELECT id FROM cats WHERE name = 'Red') FROM toys
+    WHERE cat_id NOT IN (SELECT id FROM cats WHERE name = 'Red');
 
 -- Query spoiled cats reporting the most spoiled first
 -- Your code here
+SELECT cats.name, COUNT(*) AS num_toys FROM cats JOIN toys ON cat_id = cats.id
+    GROUP BY cats.name HAVING num_toys >= 2 ORDER BY num_toys;
