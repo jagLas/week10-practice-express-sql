a401769c9d0b401d313b21fb455e6439f6a3862c
jagLas <jglasher@gmail.com>
2023-04-05 13:43:50 -0400

    sql created for phase 1

--- practice-for-week-10-combining-express-sql-long-practice/server/seed-data.sql
+++ practice-for-week-10-combining-express-sql-long-practice/server/seed-data.sql
@@ -1,5 +1,21 @@
 -- BASIC PHASE 1A - DROP and CREATE table
 -- Your code here
+DROP TABLE IF EXISTS trees;
+
+CREATE TABLE trees (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tree VARCHAR(32),
+    location VARCHAR(64),
+    height_ft NUMERIC,
+    ground_circumference_ft NUMERIC
+);
 
 -- BASIC PHASE 1B - INSERT seed data
 -- Your code here
+
+INSERT INTO trees VALUES 
+    (1,"General Sherman","Sequoia National Park",274.9,102.6),
+    (2,"General Grant","Kings Canyon National Park",268.1,107.5),
+    (3,"President","Sequoia National Park",240.9,93),
+    (4,"Lincoln","Sequoia National Park",255.8,98.3),
+    (5,"Stagg","Private Land",243,109)
